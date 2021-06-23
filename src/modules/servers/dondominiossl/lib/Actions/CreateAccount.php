f40d3811eb54f88f6f6a6c4592c6c4cf48a04bc2
JAndreuSoler <joanandreusole@gmail.com>
2021-06-23 13:00:47 +0200

    Save CertificateID in customFields

--- src/modules/servers/dondominiossl/lib/Actions/CreateAccount.php
+++ src/modules/servers/dondominiossl/lib/Actions/CreateAccount.php
@@ -8,6 +8,10 @@ class CreateAccount
     protected array $params = [];
     protected \WHMCS\Module\Server\Dondominiossl\Services\Contracts\APIService_Interface $api;
 
+    protected string $fieldCSR = \WHMCS\Module\Addon\Dondominio\Models\SSLProduct_Model::CUSTOM_FIELD_CSR;
+    protected string $fieldAdminID = \WHMCS\Module\Addon\Dondominio\Models\SSLProduct_Model::CUSTOM_FIELD_ADMIN_ID;
+    protected string $fieldCertificateID = \WHMCS\Module\Addon\Dondominio\Models\SSLProduct_Model::CUSTOM_FIELD_CERTIFICATE_ID;
+
     public function __construct(
         \WHMCS\Module\Server\Dondominiossl\Services\Contracts\APIService_Interface $api,
         array $params
@@ -18,30 +22,59 @@ class CreateAccount
 
     public function execute(): string
     {
-        if (empty($this->params['customfields']['CSR'])){
+        if (empty($this->params['customfields'][$this->fieldCSR])){
             return 'CSR Data not found';
         }
 
-        if (empty($this->params['customfields']['Admin Contact ID'])){
+        if (empty($this->params['customfields'][$this->fieldAdminID])){
             return 'Admin Contact ID not found';
         }
 
-        if (empty($this->params['configoption3'])){
+        if (empty($this->params['configoption1'])){
             return 'Product ID not found';
         }
 
         $args = [
             'productID' => $this->params['configoption3'],
             'csrData' => $this->params['customfields']['CSR'],
-            'adminContactID' => $this->params['customfields']['Admin Contact ID'],
+            'adminContactID' => $this->params['customfields'][$this->fieldAdminID],
         ];
 
+        $customFieldValue = $this->getCustomFieldValue();
+
+        if (empty($customFieldValue)){
+            return sprintf('Custom Field %s not found', $this->fieldCertificateID);
+        }
+
         try {
-            $this->api->createCertificate($args);
+            $response = $this->api->createCertificate($args);
         } catch (\Exception $e){
             return $e->getMessage();
         }
 
+        $customFieldValue->value = $response->get('ssl')['certificateID'];
+        $customFieldValue->save();
+
         return 'success';
+    }
+
+    protected function getCustomFieldValue()
+    {
+        $customField = \WHMCS\CustomField::where([
+            'relid' => $this->params['pid'],
+            'type' => 'product',
+            'fieldname' => $this->fieldCertificateID
+        ])->first();
+
+        if (empty($customField)){
+            return null;
+        }
+
+        $customFieldValue = \WHMCS\CustomField\CustomFieldValue::where([
+            'fieldid' => $customField->id,
+            'relid' => $this->params['serviceid'],
+        ])->first();
+
+        return $customFieldValue;
     }
 }
