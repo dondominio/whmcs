97859ef92a1f182f372e087c6731c84d8b96d134
JAndreuSoler <joanandreusole@gmail.com>
2021-09-07 10:25:32 +0200

    feature: add translations for .es additional fields

---
+++ src/modules/registrars/dondominio/lang/overrides/english.php
@@ -0,0 +1,9 @@
+<?php
+
+// DONDOMINIO
+$_LANG['es_admin_vat'] = 'DNI Administrative Contact';
+$_LANG['estldidformnum'] = 'NIF Contact Owner';
+$_LANG['estldlegalform'] = 'Legal Form';
+$_LANG['estldowneridtype'] = 'Owner document type';
+$_LANG['estldadminidtype'] = 'Administrator document type';
+// DONDOMINIO
