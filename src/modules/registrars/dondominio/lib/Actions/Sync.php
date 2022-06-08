bc5409e5c0c8d42bd37a36df00fdb5105ce06f41
JAndreuSoler <joanandreusole@gmail.com>
2022-06-08 16:03:32 +0200

    fix: transferAway in Domain Sync

--- src/modules/registrars/dondominio/lib/Actions/Sync.php
+++ src/modules/registrars/dondominio/lib/Actions/Sync.php
@@ -3,9 +3,15 @@
 namespace WHMCS\Module\Registrar\Dondominio\Actions;
 
 use Exception;
+use WHMCS\Domain\Domain;
 
 class Sync extends Action
 {
+    private const NOT_FOUND_DOMAIN_API_CODE = 2008;
+
+    /**
+     * IMPORTANT!: When this function is modified, synchronize with the syncDomain function in WHMCS\Module\Addon\Dondominio\Services\WHMCS_Service.
+     */
     public function __invoke()
     {
         try {
@@ -14,30 +20,28 @@ class Sync extends Action
             $result['active'] = in_array($response->get('status'), $this->getActiveStatus());
             $result['expired'] = in_array($response->get('status'), $this->getExpiredStatus());
 
-            //Adding the expiry date to the information
+            // Adding the expiry date to the information
             $expir = $response->get('tsExpir');
 
             if (!empty($expir)) {
                 $result['expirydate'] = $expir;
             }
 
-            //IDProtection Sync
+            // IDProtection Sync
             $protection = $this->params['idprotection'] ? true : false;
 
-            if ($protection != $response->get("whoisPrivacy") && $result['active'] && !$result['expired']) {
-                //Updating IDProtection
+            if ($protection != $response->get('whoisPrivacy') && $result['active'] && !$result['expired']) {
+                // Updating IDProtection
                 $fields = ['updateType' => 'whoisPrivacy', 'whoisPrivacy' => $protection];
                 $this->app->getService('api')->updateDomain($this->domain, $fields);
             }
 
             return $result;
         } catch (Exception $e) {
-            // 2008 = Domain Not Found
-            if (!$e->getCode() != 2008) {
-                return [
-                    'active' => false,
-                    'expired' => true
-                ];
+            if ((int) $e->getCode() === static::NOT_FOUND_DOMAIN_API_CODE) {
+                $this->calcNotFound($result);
+
+                return $result;
             }
 
             throw $e;
@@ -51,7 +55,7 @@ class Sync extends Action
             'renewed',
             'expired-renewgrace',
             'expired-redemption',
-            'expired-pendingdelete'
+            'expired-pendingdelete',
         ];
     }
 
@@ -60,7 +64,28 @@ class Sync extends Action
         return [
             'expired-renewgrace',
             'expired-redemption',
-            'expired-pendingdelete'
+            'expired-pendingdelete',
         ];
+    }
+
+    private function calcNotFound(&$result)
+    {
+        $domain = Domain::find($this->getParam('domainid'));
+
+        if (is_null($domain)) {
+            return;
+        }
+
+        $isExpired = $domain->expirydate->isPast();
+        $actualStatus = $domain->status;
+        $previousTransferredAwayStatus = [
+            'Active',
+            'Transferred Away',
+        ];
+
+        if (!$isExpired && in_array($actualStatus, $previousTransferredAwayStatus)) {
+            $result['active'] = false;
+            $result['transferredAway'] = true;
+        }
     }
 }
